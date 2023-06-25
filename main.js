e3840ff2b144d8728e83e5132084292145e25fe9
磯上　紡 <issotsumugi@gmail.com>
2023-06-25 12:27:34 +0900

    faze6

--- main.js
+++ main.js
@@ -39,6 +39,7 @@ document.onkeyup = function(evt){
         return ! removal.includes(v);
     });
 }
+
 //キャンバスを定義
 let can = document.getElementById("can");
 let cont = can.getContext("2d");
@@ -50,6 +51,28 @@ let vcan = document.createElement("canvas");
 let vcont = vcan.getContext("2d");
 vcan.width = FIELD_W;
 vcan.height = FIELD_H;
+
+//camera
+let camera_x = 0;
+let camera_y = 0;
+
+//random
+function random(min, max){
+    return Math.floor(Math.random()*(max - min + 1) + min)
+}
+
+//update objects
+function updateObject(objects){
+    for(let i=objects.length-1 ; i>=0 ; i--){
+        objects[i].update();
+        if(objects[i].delFlag)objects.splice(i,1);
+    }
+}
+
+//draw objects
+function drawObject(objects){
+    for(let i=0 ; i<objects.length ; i++)objects[i].draw();
+}
 
 //Sprite
 let SpriteImage = new Image();
@@ -65,13 +88,35 @@ class Sprite{
 }
 
 let sprite = [
+    //player 0~2
     new Sprite(1,0,26,30),
     new Sprite(29,0,26,30),
     new Sprite(57,0,26,30),
 
-    new Sprite(0,31,7,8),
-    new Sprite(7,31,7,11)
-]
+    //player bullets 3,4
+    new Sprite(1,34,7,7),
+    new Sprite(8,33,7,11),
+    
+    //mob green 5~7
+    new Sprite(2,45,15,16),
+    new Sprite(18,45,15,16),
+    new Sprite(2.5,77.5,6,6),
+
+    //mob yellow 8~10
+    new Sprite(38,45,15,16),
+    new Sprite(54,45,15,16),
+    new Sprite(21.5,77.5,6,6),
+
+    //mob red 11~12
+    new Sprite(2,61,15,16),
+    new Sprite(18,61,15,16),
+    new Sprite(14.5,77.5,6,6),
+
+    //mob blue 13~15
+    new Sprite(38,61,15,16),
+    new Sprite(54,61,15,16),
+    new Sprite(8.5,77.5,6,6),
+];
 
 function drawSprite(snum, x, y, s=1){
     let sx = sprite[snum].x;
@@ -82,14 +127,14 @@ function drawSprite(snum, x, y, s=1){
     let px = (x>>8) - sw/2;
     let py = (y>>8) - sh/2;
 
-    if(px+sw<camera_x || px-sw>=camera_x + SCREEN_W || py+sh<camera_y || py-sh>=camera_y + SCREEN_H) return;
+    if(px+sw*s<camera_x || px>=camera_x + SCREEN_W || py+sh*s<camera_y || py>=camera_y + SCREEN_H) return;
     vcont.drawImage(SpriteImage, sx, sy, sw, sh, px, py, sw*s, sh*s);
 }
 
-//bullet
-class Bullet{
-    constructor(x, y, vx, vy){
-        this.snum = 4;
+//charactor base
+class CharactorBase{
+    constructor(x, y, vx, vy, snum){
+        this.snum = snum;
         this.x = x;
         this.y = y;
         this.vx = vx;
@@ -104,12 +149,45 @@ class Bullet{
         if(this.x < 0 || this.x > FIELD_W<<8 || this.y <0 || this.y > FIELD_H<<8)this.delFlag = true;
     }
 
+    draw(s=1){
+        drawSprite(this.snum, this.x, this.y, s);
+    }
+}
+//bullet
+class Bullet extends CharactorBase{
+    constructor(x, y, vx, xy, snum = 3){
+        super(x, y, vx, xy, snum);
+    }
+    update(){
+        super.update();
+    }
     draw(){
-        drawSprite(this.snum, this.x, this.y);
+        super.draw();
     }
 }
 
 let bullets = [];
+
+//enemy
+class Enemy extends CharactorBase{
+    constructor(x, y, vx, vy, snum){
+        super(x, y, vx, vy, snum);
+    }
+
+    update(){//snum,num
+        super.update();
+        //this.snum = snum + num;
+    }
+
+    draw(){
+        super.draw(2);
+    }
+}
+
+let enemies = [
+    new Enemy(100<<8, 100<<8, 0, 0, 5),
+    new Enemy(200<<8, 200<<8, 0, 0, 8),
+]
 
 //player
 class Player{
@@ -124,10 +202,10 @@ class Player{
 
     update(){
         if(keys.includes('Space') && this.reload == 0){
-            bullets.push(new Bullet(this.x-(6<<8), this.y-(2<<8), 0, -1400));
-            bullets.push(new Bullet(this.x+(6 <<8), this.y-(2<<8), 0, -1400));
-            bullets.push(new Bullet(this.x-(6<<8), this.y-(2<<8), -256, -1400));
-            bullets.push(new Bullet(this.x+(6 <<8), this.y-(2<<8), 256, -1400));
+            bullets.push(new Bullet(this.x-(8<<8), this.y-(4<<8), 0, -1400));
+            bullets.push(new Bullet(this.x+(8 <<8), this.y-(4<<8), 0, -1400));
+            bullets.push(new Bullet(this.x-(8<<8), this.y-(4<<8), -256, -1400));
+            bullets.push(new Bullet(this.x+(8 <<8), this.y-(4<<8), 256, -1400));
             this.reload = 10;
             //数発打ったら遅延
             if(++this.reload2 == 4){
@@ -150,15 +228,6 @@ class Player{
 }
 
 let player = new Player;
-
-//camera
-let camera_x = 0;
-let camera_y = 0;
-
-//random
-function random(min, max){
-    return Math.floor(Math.random()*(max - min + 1) + min)
-}
 
 //star
 class Star{
@@ -198,26 +267,28 @@ function gameInit(){
     for(let i=0 ; i<STAR_MAX ; i++)stars[i] = new Star();
 }
 
-//game Loop
-function gameLoop(){
-    //移動処理
-    for(let i=0 ; i<STAR_MAX ; i++)stars[i].update();
-    for(let i=bullets.length-1 ; i>=0 ; i--){
-        bullets[i].update();
-        if(bullets[i].delFlag)bullets.splice(i,1);
-    }
+let count=0;
+num=0;
+
+//移動処理
+function upadateAll(){
+    updateObject(stars);
+    updateObject(bullets);
+    updateObject(enemies);
     player.update();
+}
 
-
-    //描画処理
+//描画処理
+function drawAll(){
     vcont.fillStyle = "black";
     vcont.fillRect(camera_x, camera_y, SCREEN_W, SCREEN_H);
-    for(let i=0 ; i<STAR_MAX ; i++)stars[i].draw();
-    for(let i=0 ; i<bullets.length ; i++)bullets[i].draw();
+    drawObject(stars);
+    drawObject(bullets);
+    drawObject(enemies);
     r = random(0,2);
     player.draw(r);
-    
-    //drawSprite(3, 100<<8, 100<8,10);
+
+    //drawSprite(5, 100<<8, 100<8);
 
     //player 0 ~ FIELD_W
     //camera 0 ~ FIELD_W - SCREEN_W
@@ -226,8 +297,10 @@ function gameLoop(){
 
     //仮装画面から実際のキャンバスにコピー
     cont.drawImage(vcan, camera_x, camera_y, SCREEN_W, SCREEN_H, 0, 0, CANVAS_W, CANVAS_H);
+}
 
-
+//デバッグ情報表示
+function debugShow(){
     if(DEBUG){
         drawCount++;
         if(lastTime + 1000 <= Date.now()){
@@ -239,7 +312,17 @@ function gameLoop(){
         cont.fillStyle = "white";
         cont.fillText('FPS:'+fps, 120, 20)
         cont.fillText('Bullets:'+bullets.length, 20, 20)
+        cont.fillText('Enemies:'+enemies.length, 20, 40)
     }
+}
+
+//game Loop
+function gameLoop(){
+    upadateAll();
+
+    drawAll();
+
+    debugShow();
     //ループ終了
 }
 
